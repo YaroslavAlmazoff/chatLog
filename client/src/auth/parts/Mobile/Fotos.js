d773ae5db88360380b532d5e03869303341ed528
Yaroslav <yaroslav.almazoff@mail.ru>
2022-06-10 21:37:35 +0300

    1

--- client/src/auth/parts/Mobile/Fotos.js
+++ client/src/auth/parts/Mobile/Fotos.js
@@ -22,9 +22,6 @@ const Fotos = ({userFotos, isOwner, showNotifications, getFile2, emitOpen2, file
             <div className="fotos-actions">
                 <p className="user-fotos-title-mobile">Фотографии {userFotos.length}</p>
                 {isOwner ? <div>
-                        <div>
-                            <img className="notice-img" onClick={showNotifications} width="35" src={require('../img/notice.png')} alt="notice" />
-                        </div>
                         <input onChange={(e) => getFile2(e)} ref={fileRef2} type="file" />
                         <button onClick={(e) => emitOpen2(e)} className="user-add-foto-right">Добавить фотографию</button>
                         <button onClick={() => sendFoto(file2, getCurrentDate, params, userFotos, setUserFotos)} className="user-add-foto-right">Отправить фотографию</button>
