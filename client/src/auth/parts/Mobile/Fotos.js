f12085158c243fe7b49906a61d132d5edb93dd7f
Yaroslav <yaroslav.almazoff@mail.ru>
2022-06-10 18:59:24 +0300

    1

--- client/src/auth/parts/Mobile/Fotos.js
+++ client/src/auth/parts/Mobile/Fotos.js
@@ -3,6 +3,7 @@ import useRandom from "../../../common_hooks/random.hook";
 import "../../styles/user.css"
 import { useNavigate } from "react-router";
 import FotoItem from "../FotoItem";
+import FotoItemMobile from "./FotoItemMobile";
 
 const Fotos = ({userFotos}) => {
     //Правая часть страницы пользователя - добавление фотографий и список фотографий
@@ -14,7 +15,7 @@ const Fotos = ({userFotos}) => {
     return (
         <div className="user-fotos-mobile">
             <p className="user-fotos-title-mobile">Фотографии {userFotos.length}</p>
-            {userFotos.map(el => <div className="foto-div-mobile" onClick={() => showFotography(el.imageUrl)} key={randomKey()}><FotoItem imageUrl={el.imageUrl} /></div>)}
+            {userFotos.map(el => <div className="foto-div-mobile" onClick={() => showFotography(el.imageUrl)} key={randomKey()}><FotoItemMobile imageUrl={el.imageUrl} /></div>)}
         </div>
     )
 }
