7b6853b6aeba40e20bb28fcc36a89a98388edbab
Yaroslav <yaroslav.almazoff@mail.ru>
2022-06-13 23:00:16 +0300

    0

--- client/src/auth/parts/UserPost.js
+++ client/src/auth/parts/UserPost.js
@@ -98,7 +98,7 @@ const UserPost = ({title, date, imageUrl = 'user.png', likes, comments, id, dele
         <div ref={articleRef} onMouseOver={() => updateLikers()}  onMouseLeave={() => setLikersDisplay('none')} onClick={() => openPost(obj)} className={imageUrl !== 'none.png' ? "article" : "article-without-image"}> 
             {imageUrl !== 'none.png' ? <div className="portrait-crop">
                 <img className="article-image" src={imageCode} alt="article"/>
-            </div> : <h2 className="title">{divideWord(title, 100)}</h2> }
+            </div> : <h2 className="title">{divideWord(title, 200)}</h2> }
              
             <div className={imageUrl !== 'none.png' ? "info": "info-without-image"}>
             {isOwner ? <p onClick={imageUrl.split('.')[1] !== 'mp4' ? (e) => deletePost(e, obj.id, setUserPosts, userPosts) : (e) => deleteVideo(e, obj.id, setUserVideos, userVideos)} className={imageUrl.split('.')[1] !== 'mp4' ? "delete-user-post" : "delete-user-video"}>&times;</p> : <></>}
