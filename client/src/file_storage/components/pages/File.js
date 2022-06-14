cdf813952225d099bd6c00a9456dcad3c75cea9a
Yaroslav <yaroslav.almazoff@mail.ru>
2022-06-14 21:54:18 +0300

    0

--- client/src/file_storage/components/pages/File.js
+++ client/src/file_storage/components/pages/File.js
@@ -55,6 +55,9 @@ const File = () => {
             ? <div className="file-wrapper">
                 {file.public || file.owner === auth.userId
                 ? <div className="file">
+                    {file.ext !== 'png' || file.ext !== 'jpg' || file.ext !== 'gif' || file.ext !== 'bmp' || file.ext !== 'avi' || file.ext !== 'mp4'
+                    ? <br /> : <></>
+                    }
                     <FilePreview file={file} fileText={fileText} fileOpened={true} ready={ready} />
                 </div>
                 : <div>
