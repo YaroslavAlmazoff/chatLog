28900f28dbaa36059114c16527968a8a4f98e8ac
Yaroslav <yaroslav.almazoff@mail.ru>
2022-06-17 20:30:21 +0300

    1

--- client/src/messenger/components/pages/Room.js
+++ client/src/messenger/components/pages/Room.js
@@ -57,7 +57,9 @@ export const Room = () => {
             console.log(localStorage.getItem('file-link'))
             setMessage(<Link to={localStorage.getItem('file-link')}>Файл {localStorage.getItem('file-link')}</Link>)
             localStorage.removeItem('file-link')
+            console.log(message.includes('<'))
         }
+        
     }, [])
     const subscribe = async () => {
         
