4aaa17e09501fa110e442f2f0e3a5754495a49bb
Yaroslav <yaroslav.almazoff@mail.ru>
2022-06-17 23:04:29 +0300

    1

--- client/src/messenger/components/parts/Message.js
+++ client/src/messenger/components/parts/Message.js
@@ -33,7 +33,7 @@ const Message = ({mess}) => {
                             <div className="message-text">
                                 {
                                 mess.isFile 
-                                ? <a className="message-file-link" href={mess.message.substr(20)}>Файл {mess.message}</a>
+                                ? <a className="message-file-link" href={mess.message.substr(20, mess.message.length - 20)}>Файл {mess.message}</a>
                                 : 
                                 <p>{mess.message}</p>
                                 }
