4481c5ce6e96b2122fef34b6dfaba9239ad406d3
Yaroslav <yaroslav.almazoff@mail.ru>
2022-06-20 17:43:43 +0300

    1

--- client/src/landing_page/SupportPage.js
+++ client/src/landing_page/SupportPage.js
@@ -1,7 +1,19 @@
-const SupportPage = () => {
+import { useState } from "react"
+import api from '../auth/api/auth'
+import './support-page.css'
 
+const SupportPage = () => {
+    const [message, setMessage] = useState('Ваше сообщение...')
+    const sendMessage = async () => {
+        const response = await api.post('/api/support')
+        console.log(response)
+    }
     return (
-        <div></div>
+        <div className='support_page'>
+            <h3>Подробно опишите проблему:</h3>
+            <textarea className="support_page_area" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+            <button onClick={sendMessage} className="support_page_send">Отправить</button>
+        </div>
     )
 }
 
