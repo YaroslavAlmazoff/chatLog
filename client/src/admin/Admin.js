89b2c8792492b37d376efa8828d3ec9489515be9
Yaroslav <yaroslav.almazoff@mail.ru>
2022-06-20 21:07:08 +0300

    1

--- client/src/admin/Admin.js
+++ client/src/admin/Admin.js
@@ -11,8 +11,8 @@ const Admin = () => {
     const [visits, setVisits] = useState(0)
 
     useEffect(() => {
-        console.log(auth.userId === null || auth.userId !== '628e5aab0153706a3e18fe79')
-        if(auth.userId === null || auth.userId !== '628e5aab0153706a3e18fe79') {
+        console.log(auth.userId !== '628e5aab0153706a3e18fe79', auth.userId)
+        if(auth.userId !== '628e5aab0153706a3e18fe79') {
             navigate('/')
         }
     }, [auth])
