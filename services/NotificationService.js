40747159726cf3128ea77cd37728102f0e6595a6
Yaroslav <yaroslav.almazoff@mail.ru>
2022-06-22 22:06:13 +0300

    0

--- services/NotificationService.js
+++ services/NotificationService.js
@@ -19,6 +19,7 @@ class NotificationService {
         const notifications = await Notification.find({to: user})
 
         await Notification.findByIdAndUpdate(notifications[notifications.length - 1], {checked: true})
+        console.log('checked')
         res.json({msg: 'success'})
     }
     //Возращение уведомлений пользователя на клиент
