b5d75d98dcc143ac513d3c169513647815c8b51f
Yaroslav <yaroslav.almazoff@mail.ru>
2022-06-23 19:45:03 +0300

    0

--- client/src/file_storage/components/pages/FileStorage.js
+++ client/src/file_storage/components/pages/FileStorage.js
@@ -4,6 +4,7 @@ import FileDetail from '../parts/FileDetail'
 import '../../styles/file-storage.css'
 import api from '../../../auth/api/auth'
 import { AuthContext } from '../../../context/AuthContext'
+import Loader from '../../../common_components/Loader'
 
 const FileStorage = () => {
     const auth = useContext(AuthContext)
@@ -12,23 +13,26 @@ const FileStorage = () => {
     const [detailDisplay, setDetailDisplay] = useState('none')
     const [downloadingFile, setDownloadingFile] = useState(require('../../../static/useravatars/user.png'))
     const [filePreviewDisplay, setFilePreviewDisplay] = useState('none')
+    const [filesLoading, setFilesLoading] = useState(false)
     useEffect(() => {
+        setFilesLoading(true)
         const getFiles =  async () => {
             const response = await api.get(`/api/cloud/files`, {headers: {
                 Authorization: `Bearer ${auth.token}`
             }})
             setFiles(response.data.files)
+            setFilesLoading(false)
         }
         getFiles()
     }, [auth])
     return (
         <div className='file-storage'>
-            <Files
+            {!filesLoading ? <Files
                 setFilePreviewDisplay={setFilePreviewDisplay} 
                 setSelectedFile={setSelectedFile} 
                 files={files} setFiles={setFiles} 
                 setDetailDisplay={setDetailDisplay} 
-            />
+            /> : <Loader ml={'50%'} />}
             <div className='file-detail-wrapper'><FileDetail 
                 setFiles={setFiles} 
                 downloadingFile={downloadingFile}
