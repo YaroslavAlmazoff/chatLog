bbbcfac41498f084a9fd8b64d74a4c160bc7d130
Yaroslav <yaroslav.almazoff@mail.ru>
2022-06-25 22:22:01 +0300

    0

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
     setVisit()
     const verify = async () => {
       const response = await api.get('/verify', {headers:{
-        Authorization: `Bearer ${JSON.parse(localStorage.getItem('user').token)}`
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem('user')).token}`
       }})
       setIsVerified(response.data.verified)
       console.log(isVerified, isAuthenticated)
