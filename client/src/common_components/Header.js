78f3b8f0bf5037ed888ae7b9cc9b58a6337dc3de
Yaroslav <yaroslav.almazoff@mail.ru>
2022-06-27 18:15:56 +0300

    0

--- client/src/common_components/Header.js
+++ client/src/common_components/Header.js
@@ -26,7 +26,7 @@ const Header = ({isVerified}) => {
         <div className="header">
             <h2 className="logo">CHATLOG.RU</h2>
             <div className="links">
-                {auth.isAuthenticated && isVerified 
+                {auth.isAuthenticated || isVerified 
                 ?   <div className="links">
                         <NavLink ref={linkRef1} onClick={() => visit(linkRef1)} className="link" to="/">Главная</NavLink>
                         <NavLink ref={linkRef8} onClick={() => visit(linkRef8)} className="link" to={`/user/${auth.userId}`}>Мой профиль</NavLink>
