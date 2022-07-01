7747d05b443ed32b80534f32b73a89ff5a52fd94
Yaroslav <yaroslav.almazoff@mail.ru>
2022-07-01 18:05:53 +0300

    0

--- client/src/messenger/components/pages/smiles.js
+++ client/src/messenger/components/pages/smiles.js
@@ -1,4 +1,4 @@
 export const smiles = [
-    {id: 1, imageUrl: require(`../../img/smiles/${this.id}.png`)},
-    {id: 2, imageUrl: require(`../../img/smiles/${this.id}.png`)},
+    {id: 1},
+    {id: 2},
 ]
--- client/src/messenger/components/parts/Smile.js
+++ client/src/messenger/components/parts/Smile.js
@@ -3,7 +3,7 @@ import '../styles/smiles.css'
 const Smile = ({el}) => {
     return (
         <div className="smile-wrapper">
-            <img className="smile" src={el.imageUrl} alt="smile" />
+            <img className="smile" src={require(`../../img/smiles/${el.id}.png`)} alt="smile" />
         </div>
     )
 }
