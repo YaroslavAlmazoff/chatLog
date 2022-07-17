e4b82b19f4971a3742682266fd9b12cd718fe63e
Yaroslav <yaroslav.almazoff@mail.ru>
2022-07-17 23:46:05 +0300

    0

--- client/src/inner_ad/components/components/Card.js
+++ client/src/inner_ad/components/components/Card.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Card = ({item}) => {
     return (
         <div className="inner-ad-item">
-            <Link style={{textDecoration: 'none'}} to={item.link}>
+            <Link className='sd-none' to={item.link}>
                 <img src={process.env.REACT_APP_API_URL + `/inneradimages/${item.imageUrl}`} alt="inner-ad-item-img" className='inner-ad-item-img' />
                 <p className='inner-ad-item-title'>{item.title}</p>
             </Link>
