e99c97deadb178d5994d1bbe1add6272f35d0870
Yaroslav <yaroslav.almazoff@mail.ru>
2022-07-17 22:16:35 +0300

    1

--- models/InnerAd.js
+++ models/InnerAd.js
@@ -4,7 +4,7 @@ const InnerAd = new mongoose.Schema({
     title: {type: String, required: true},
     text: {type: String},
     imageUrl: {type: String},
-    link: {tyle: String, required: true},
+    link: {type: String, required: true},
     date: {type: String, required: true},
     active: {type: Boolean, required: true}
 })
