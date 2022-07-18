329fd0ed3255cefa773b957c94d10a76a8335834
Yaroslav <yaroslav.almazoff@mail.ru>
2022-07-18 17:16:59 +0300

    1

--- client/src/inner_ad/components/components/ShowAdLeft.js
+++ client/src/inner_ad/components/components/ShowAdLeft.js
@@ -10,6 +10,19 @@ const ShowAd = ({width}) => {
             const response = await api.get('/api/innerad/all')
             console.log(response)
             setAds(response.data.ads)
+            setInterval(() => {
+                function compare( a, b ) {
+                    if ( a.title < b.title ){
+                      return -1;
+                    }
+                    if ( a.title > b.title ){
+                      return 1;
+                    }
+                    return 0;
+                }
+                const newads = ads.sort(compare)
+                setAds(newads)
+            },10000)
         }
         getAds()
     }, [])
