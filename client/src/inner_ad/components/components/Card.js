41e5542b7be0917f1eccc38ee14b3626f4726f15
Yaroslav <yaroslav.almazoff@mail.ru>
2022-07-19 21:52:42 +0300

    1

--- client/src/inner_ad/components/components/Card.js
+++ client/src/inner_ad/components/components/Card.js
@@ -17,7 +17,11 @@ const Card = ({item}) => {
         e.stopPropagation()
         const response = await api.get(`/api/innerad/click/${item._id}`)
         console.log(response)
-        linkRef.current.click()
+        if(item.link) {
+            window.location = item.link
+        } else {
+            window.location = `/innerad/${item._id}`
+        }
     }
 
     return (
@@ -26,7 +30,7 @@ const Card = ({item}) => {
                 <img src={process.env.REACT_APP_API_URL + `/inneradimages/${item.imageUrl}`} alt="inner-ad-item-img" className='inner-ad-item-img' />
                 <p className='inner-ad-item-title'>{item.title}</p>
             </a> :
-            <a ref={linkRef} className='sd-none' href={`/innerad/${item.id}`}>
+            <a ref={linkRef} className='sd-none' href={`/innerad/${item._id}`}>
                 <img src={process.env.REACT_APP_API_URL + `/inneradimages/${item.imageUrl}`} alt="inner-ad-item-img" className='inner-ad-item-img' />
                 <p className='inner-ad-item-title'>{item.title}</p>
             </a>
