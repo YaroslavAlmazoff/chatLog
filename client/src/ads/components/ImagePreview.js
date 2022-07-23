3a6817fbe5d4d37eb23ca80d26b538dd16b439ba
Yaroslav <yaroslav.almazoff@mail.ru>
2022-07-23 19:28:38 +0300

    0

--- client/src/ads/components/ImagePreview.js
+++ client/src/ads/components/ImagePreview.js
@@ -3,7 +3,7 @@ import React from "react";
 const ImagePreview = ({imageUrl}) => {
     //Предпросмотр изображения для загрузки нового поста
     return (
-        <div style={{height: '100%'}}>
+        <div style={{height: '100%',display: 'inline'}}>
             <img height="200" src={imageUrl} alt="preview" style={{height: '100%'}} />
         </div>
     )
