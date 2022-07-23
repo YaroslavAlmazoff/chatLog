b5bcaab7ce55256c718eac1e3edd635f0b05f4d9
Yaroslav <yaroslav.almazoff@mail.ru>
2022-07-23 22:06:23 +0300

    0

--- client/src/ads/pages/AdMain.js
+++ client/src/ads/pages/AdMain.js
@@ -1,10 +1,16 @@
 import Head from "../components/Head"
 import '../styles/main.css'
+import AdCity from "./AdCity"
+import AdNew from "./AdNew"
 
 const AdMain = () => {
     return (
         <div className="ads-main">
-            <Head />
+            <div className="ads-main-content">
+                <Head />
+                <AdNew />
+                <AdCity />
+            </div>
         </div>
     )
 }
