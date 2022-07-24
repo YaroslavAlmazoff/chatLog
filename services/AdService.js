86395f3b345a56849df38c4ff66517a1fe9e586c
Yaroslav <yaroslav.almazoff@mail.ru>
2022-07-24 18:54:43 +0300

    1

--- services/AdService.js
+++ services/AdService.js
@@ -23,10 +23,9 @@ class AdService {
     }
     async new(req, res) {
         const allAds = await Ad.find({})
-        console.log(req.body.date)
-        const ads = allAds.filter(el => {
+        const ads = allAds.filter(el => el.date.split('.')[0] === req.body.date.split('.')[0])
+        allAds.forEach(el => {
             console.log(el.date, req.body.date, el.date === req.body.date)
-            return el.date.split('.')[0] === req.body.date.split('.')[0]
         })
         res.json({ads})
     }
