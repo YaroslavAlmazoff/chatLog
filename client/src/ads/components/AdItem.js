96298fc8222278fecefba319dea7fa4169bcf1e7
Yaroslav <yaroslav.almazoff@mail.ru>
2022-07-24 19:37:27 +0300

    0

--- client/src/ads/components/AdItem.js
+++ client/src/ads/components/AdItem.js
@@ -6,8 +6,8 @@ const AdItem = ({item}) => {
             <img className="ad-item-image" src={process.env.REACT_APP_API_URL + '/ads/' + item.images[0]} alt="ad" />
             <div className="ad-item-info">
                 <p className="ad-item-title">{item.title}</p>
-                <span style={{color: 'white'}}>Стоимость: </span><p className="ad-item-price">{item.price}&#8381;</p>
-                <span style={{color: 'white'}}>Город: </span><p className="ad-item-city">{item.city}</p>
+                <p className='ad-item-price'><span style={{color: 'white'}}>Стоимость: </span><span>{item.price}&#8381;</span></p>
+                <p className="ad-item-city"><span style={{color: 'white'}}>Город: </span><span>{item.city}</span></p>
                 <p className="ad-item-date">{item.date}</p>
             </div>
         </div>
