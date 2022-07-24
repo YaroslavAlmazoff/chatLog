acf7f697b41e9692593cb17b096498bb73de1858
Yaroslav <yaroslav.almazoff@mail.ru>
2022-07-24 17:19:25 +0300

    0

--- client/src/ads/pages/AdMain.js
+++ client/src/ads/pages/AdMain.js
@@ -1,15 +1,15 @@
+import CityBlock from "../components/CityBlock"
 import Head from "../components/Head"
+import NewBlock from "../components/NewBlock"
 import '../styles/main.css'
-import AdCity from "./AdCity"
-import AdNew from "./AdNew"
 
 const AdMain = () => {
     return (
         <div className="ads-main">
             <div className="ads-main-content">
                 <Head />
-                <AdNew />
-                <AdCity />
+                <NewBlock />
+                <CityBlock />
             </div>
         </div>
     )
