56fe3eca2c592925c0fb8caf980cf0f8de25fbe0
Yaroslav <yaroslav.almazoff@mail.ru>
2022-07-26 23:40:48 +0300

    1

--- client/src/homepage/components/Forecast.js
+++ client/src/homepage/components/Forecast.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import api from '../../auth/api/auth'
 import { AuthContext } from "../../context/AuthContext"
 import useWord from "../../common_hooks/divideWord.hook"
@@ -7,6 +7,9 @@ import useWord from "../../common_hooks/divideWord.hook"
 const Forecast = () => {
     const {translit} = useWord()
     const auth = useContext(AuthContext)
+
+    const [weather, setWeather] = useState(null)
+    const [city, setCity] = useState('')
     
     useEffect(() => {
         if(!auth.token) return
@@ -15,24 +18,38 @@ const Forecast = () => {
             const URL = 'https://api.openweathermap.org/data/2.5/weather'
             const API_KEY = '928ba56fdc47f5fe29a01f2ae34f87f3'
             const response1 = await api.get(`/api/user/${auth.userId}`)
+            setCity(response1.data.user.city)
             if(response1.data.user.city) {
-                const response2 = await axios.get(URL, {
-                    params: {
-                        q: translit(response1.data.user.city),
-                        units: 'metric',
-                        APPID: API_KEY
-                    }
-                })
-                console.log(response2)
+                try {
+                    const response2 = await axios.get(URL, {
+                        params: {
+                            q: `${translit(response1.data.user.city)},RU`,
+                            units: 'metric',
+                            APPID: API_KEY
+                        }
+                    })
+                    setWeather(response2.data)
+                } catch(e) {
+                    console.log(e)
+                }
+                
             }
         }
         getWeather()
     }, [auth])
 
     return (
-        <div className="homepage-forecast">
-
-        </div>
+        <>
+            {weather.main && 
+            <div className="homepage-forecast">
+                <p className="homepage-forecast-city">{city}</p>
+                <p>
+                    <strong className="homepage-forecast-temp">{Math.round(weather.main.temp)}&deg;, </strong>
+                    <span className="homepage-forecast-text">{weather.weather[0].main}</span>
+                </p>
+                <img src={`https://api.openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="" />
+            </div>}
+        </>
     )
 }
 
