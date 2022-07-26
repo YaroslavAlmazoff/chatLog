12070743b894733600b9cb7c4cfaece875a808ca
Yaroslav <yaroslav.almazoff@mail.ru>
2022-07-26 21:11:32 +0300

    1

--- client/src/homepage/components/Forecast.js
+++ client/src/homepage/components/Forecast.js
@@ -1,6 +1,39 @@
+import axios from "axios"
+import { useContext, useEffect } from "react"
+import api from '../../auth/api/auth'
+import { AuthContext } from "../../context/AuthContext"
+
 const Forecast = () => {
+    const auth = useContext(AuthContext)
+    
+    useEffect(() => {
+        if(!auth.token) return
+        const getWeather = async () => {
+            const URL = 'https://api.openweathermap.org/data/2.5/weather'
+            const API_KEY = '928ba56fdc47f5fe29a01f2ae34f87f3'
+            const response1 = await api.get('/api/user', {
+                headers:{
+                    Authorization: `Bearer ${auth.token}`
+                }
+            })
+            if(response1) {
+                const response2 = await axios.get(URL, {
+                    params: {
+                        q: response1.user.city,
+                        units: 'metric',
+                        APPID: API_KEY
+                    }
+                })
+                console.log(response2)
+            }
+        }
+        getWeather()
+    }, [auth])
+
     return (
-        <div></div>
+        <div className="homepage-forecast">
+
+        </div>
     )
 }
 
