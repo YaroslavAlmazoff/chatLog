124359aae6648da5528ec071f24ce9da788b4696
Yaroslav <yaroslav.almazoff@mail.ru>
2022-08-01 23:07:42 +0300

    0

--- client/src/videohost/components/components/ChannelVideos.js
+++ client/src/videohost/components/components/ChannelVideos.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import {useParams} from 'react-router'
 import api from '../../../auth/api/auth'
-import VideoItem from './components/'
+import VideoItem from './components/VideoItem'
 
 const ChannelVideos = () => {
     const params = useParams()
